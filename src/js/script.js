db7659a08093ebe7e11e1a50ecbc53b46ae0d25c
cheshochek <anna_cheshok@mail.ru>
2022-09-02 02:56:51 +0300

    add effects for icons

--- src/js/script.js
+++ src/js/script.js
@@ -3,4 +3,19 @@ $(document).ready(function () {
         $('.header__burger,.menu__list').toggleClass('active');
         $('body').toggleClass('lock');
     });
+    $('.icon-facebook').on('mouseover', function () {
+        $(this).parent().addClass('is-hover--facebook');
+    }).on('mouseout', function () {
+        $(this).parent().removeClass('is-hover--facebook');
+    })
+    $('.icon-twitter').on('mouseover', function () {
+        $(this).parent().addClass('is-hover--twitter');
+    }).on('mouseout', function () {
+        $(this).parent().removeClass('is-hover--twitter');
+    })
+    $('.icon-linkedin').on('mouseover', function () {
+        $(this).parent().addClass('is-hover--linkedin');
+    }).on('mouseout', function () {
+        $(this).parent().removeClass('is-hover--linkedin');
+    })
 });
