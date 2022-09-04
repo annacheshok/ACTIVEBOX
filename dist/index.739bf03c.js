28fa8d3c14ea5e896ccff28bc4113c8e58b15283
cheshochek <anna_cheshok@mail.ru>
2022-09-04 20:58:11 +0300

    Edit files

--- dist/index.739bf03c.js
+++ dist/index.739bf03c.js
@@ -553,8 +553,11 @@ new Swiper(".image-slider", {
 });
 $(document).ready(function() {
     $(".header__burger").click(function(event) {
-        $(".header__burger,.menu__list").toggleClass("active");
         $("body").toggleClass("lock");
+        $(".header__burger,.menu__list").toggleClass("active");
+        $(".menu-item").click(function(event) {
+            $(".header__burger,.menu__list").removeClass("active");
+        });
     });
     $(".icon").on("mouseover", function() {
         $(this).addClass("is-hover");
